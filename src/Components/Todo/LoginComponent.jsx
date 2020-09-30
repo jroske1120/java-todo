d896b82426ed1c0b6ffa359bb3eae289e75cde95
Joel Roske <joel.j.roske@gmail.com>
2020-09-30 09:14:43 -0500

    login validation hard coded

--- src/Components/Todo/LoginComponent.jsx
+++ src/Components/Todo/LoginComponent.jsx
@@ -1,35 +1,65 @@
 import React, { Component } from "react";
 
- class LoginComponent extends Component {
-   state = {
-     username: '',
-     password: '',
-   }
+class LoginComponent extends Component {
+  state = {
+    username: "",
+    password: "",
+    loginFailed: false,
+    successMessage: "",
+  };
 
-   handleChange = ( event, propertyName ) => {
-    this.setState( {
-        [propertyName]: event.target.value
-      
-    } );
-  }
-    render() {
-      return (
-        <div>
-      Username: 
-      <input 
-      type="text" name="username" 
-      value={this.state.username} 
-      onChange={ event => this.handleChange(event, 'username') } 
-      />
-      Password: 
-      <input type="password" name="password" 
-      value={this.state.password} 
-      onChange={ event => this.handleChange(event, 'password') } 
-      />
-      <button>Login</button>
-      </div>
-      );   
+  handleChange = (event, propertyName) => {
+    this.setState({
+      [propertyName]: event.target.value,
+    });
+  };
+
+  Login = () => {
+    console.log("this.state...", this.state);
+    const s = this.state;
+    if (s.username === "joel" && s.password === "joel") {
+      this.setState({ successMessage: true, loginFailed: false });
+    } else {
+      this.setState({ loginFailed: true, successMessage: false });
     }
+  };
+  render() {
+    return (
+      <div>
+        <ShowInvalidCredentials loginFailed={this.state.loginFailed} />
+        <ShowLoginSuccessMessage successMessage={this.state.successMessage} />
+        Username:
+        <input
+          type="text"
+          name="username"
+          value={this.state.username}
+          onChange={(event) => this.handleChange(event, "username")}
+        />
+        Password:
+        <input
+          type="password"
+          name="password"
+          value={this.state.password}
+          onChange={(event) => this.handleChange(event, "password")}
+        />
+        <button onClick={this.Login}>Login</button>
+      </div>
+    );
   }
-  
-  export default LoginComponent;
+}
+
+function ShowInvalidCredentials(props) {
+  if (props.loginFailed) {
+    return <div>Invalid Login</div>;
+  } return null;
+}
+
+function ShowLoginSuccessMessage(props) {
+  if (props.successMessage) {
+    return         <div>Login Successful</div>
+    ;
+  } return null;
+}
+
+
+export default LoginComponent;
