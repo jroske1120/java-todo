50a598d03902f7cc09795032772c847c53d5ac32
Joel Roske <joel.j.roske@gmail.com>
2020-10-04 11:35:19 -0500

    basic connection between java BE and React FE

--- src/Components/Todo/WelcomeComponent.jsx
+++ src/Components/Todo/WelcomeComponent.jsx
@@ -1,27 +1,38 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 // import {BrowserRouter as Router, Route} from 'react-router-dom';
-
+import HelloWorldService from "../../api/todo1/HelloWorldService.js";
 class WelcomeComponent extends Component {
+  state = {
+    welcomeMessage: "",
+  };
 
   retrieveWelcomeMessage() {
-    console.log('retrieveWelcomeMessage clicked')
+    HelloWorldService.executeHelloWorldService().then((response) =>
+      console.log("response", response)
+    );
+    // .catch()
   }
-  
+
   render() {
     return (
       <>
         <h1>Welcome!</h1>
         <div className="container">
-          Welcome {this.props.match.params.name}. 
-          You can manage your list
+          Welcome {this.props.match.params.name}. You can manage your list
           <Link to="/todos">here</Link>
         </div>
         <div className="container">
-          Click ehre for customized welcome message. 
-          <button 
-          className="btn btn-success"
-          onClick={this.retrieveWelcomeMessage}> Get Welcome message</button>
+          Click ehre for customized welcome message.
+          <button
+            className="btn btn-success"
+            onClick={this.retrieveWelcomeMessage}
+          >
+            Get Welcome message
+          </button>
+        </div>
+        <div className="container">
+          {this.state.welcomeMessage}
         </div>
       </>
     );
