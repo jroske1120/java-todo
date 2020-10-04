7e429f8eb67f5f51e21312ec9cfb588da05225f8
Joel Roske <joel.j.roske@gmail.com>
2020-10-04 11:14:42 -0500

    start link from java to react

--- src/Components/Todo/FooterComponent.jsx
+++ src/Components/Todo/FooterComponent.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import AuthenticationService from "./AuthenticationService";
-import { Route, Redirect } from "react-router-dom";
+// import AuthenticationService from "./AuthenticationService";
+// import { Route, Redirect } from "react-router-dom";
 
 
 class FooterComponent extends Component {
--- src/Components/Todo/WelcomeComponent.jsx
+++ src/Components/Todo/WelcomeComponent.jsx
@@ -3,13 +3,25 @@ import { Link } from "react-router-dom";
 // import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 class WelcomeComponent extends Component {
+
+  retrieveWelcomeMessage() {
+    console.log('retrieveWelcomeMessage clicked')
+  }
+  
   render() {
     return (
       <>
         <h1>Welcome!</h1>
         <div className="container">
-          Welcome {this.props.match.params.name}. You can manage your list
+          Welcome {this.props.match.params.name}. 
+          You can manage your list
           <Link to="/todos">here</Link>
+        </div>
+        <div className="container">
+          Click ehre for customized welcome message. 
+          <button 
+          className="btn btn-success"
+          onClick={this.retrieveWelcomeMessage}> Get Welcome message</button>
         </div>
       </>
     );
