314cca501b2476bce7212da392937cc0e75a1ebb
Joel Roske <joel.j.roske@gmail.com>
2020-10-18 12:36:11 -0500

    hardcoded basic auth in authenticationsService.js

--- src/api/todo1/HelloWorldService.js
+++ src/api/todo1/HelloWorldService.js
@@ -6,8 +6,17 @@ class HelloWorldService {
         // console.log('executed service')
     }
     executeHelloWorldPathVariableService(name) {
-        return axios.get(`http://localhost:8080/hello-world/path-variable/${name}`)
+        let username = 'joel'
+        let password = 'joel'
+        
+        let basicAuthHeader = 'Basic ' + window.btoa(username + ":" + password)
+        return axios.get(`http://localhost:8080/hello-world/path-variable/${name}`,
+        {headers : {
+            authorization: basicAuthHeader
+        }}
+        )
         // console.log('executed service')
     }
+    
 }
 export default new HelloWorldService()
