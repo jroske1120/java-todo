9a91779c3d70fc4b8064a39661ce8dcce5285353
Joel Roske <joel.j.roske@gmail.com>
2020-10-20 13:21:42 -0500

    auth refactor

---
+++ demo/src/main/java/com/example/demo/basic/auth/BasicAthenticationController.java
@@ -0,0 +1,19 @@
+package com.example.demo.basic.auth;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller
+@RestController
+@CrossOrigin(origins="http://localhost:4200")
+public class BasicAthenticationController {
+
+
+	@GetMapping(path="/basicauth")
+	public AuthenticationBean helloWorldBean() {
+		return new AuthenticationBean("You are authenticated");
+	}
+	
+}
