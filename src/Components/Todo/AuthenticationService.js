1577e7ce60de3c051a3ddc253fb6ea2d9416dd0a
Joel Roske <joel.j.roske@gmail.com>
2020-10-23 15:01:55 -0500

    configured front end jwt token creation

--- src/Components/Todo/AuthenticationService.js
+++ src/Components/Todo/AuthenticationService.js
@@ -1,28 +1,46 @@
 // import React, { Component } from "react";
 import axios from "axios";
+import {API_URL} from '../../Constants'
+export const USER_NAME_SESSION_ATTRIBUTE_NAME = 'authenticatedUser'
 class AuthenticationService {
   executeBasicAuthenticationService(username, password) {
-    return axios.get("http://localhost:8080/basicauth", {
+    return axios.get(`${API_URL}/basicauth`, {
       headers: { authorization: this.createBasicAuthToken(username, password) },
     });
   }
 
-  //refactored auth into a method to call rather than recoding 
+  executeJwtAuthenticationService(username, password) {
+    return axios.post(`${API_URL}/authenticate`, {
+      username,
+      password,
+    });
+  }
+
+  //refactored auth into a method to call rather than recoding
   createBasicAuthToken(username, password) {
     return "Basic " + window.btoa(username + ":" + password);
   }
 
   registerSuccessfulLogin(username, password) {
     // let basicAuthHeader = "Basic " + window.btoa(username + ":" + password);
-    sessionStorage.setItem("authenticatedUser", username);
+    sessionStorage.setItem(USER_NAME_SESSION_ATTRIBUTE_NAME, username);
     this.setupAxiosInterceptors(this.createBasicAuthToken(username, password));
   }
   logout() {
-    sessionStorage.removeItem("authenticatedUser");
+    sessionStorage.removeItem(USER_NAME_SESSION_ATTRIBUTE_NAME);
+  }
+
+  createJWTToken(token) {
+    return "Bearer " + token;
+  }
+
+  registerSuccessfulLoginForJwt(username, token) {
+    sessionStorage.setItem(USER_NAME_SESSION_ATTRIBUTE_NAME, username);
+    this.setupAxiosInterceptors(this.createJWTToken(token));
   }
 
   isUserLoggedIn() {
-    let user = sessionStorage.getItem("authenticatedUser");
+    let user = sessionStorage.getItem(USER_NAME_SESSION_ATTRIBUTE_NAME);
     if (user === null) {
       return false;
     } else {
@@ -31,7 +49,7 @@ class AuthenticationService {
   }
 
   getLoggedInUserName() {
-    let user = sessionStorage.getItem("authenticatedUser");
+    let user = sessionStorage.getItem(USER_NAME_SESSION_ATTRIBUTE_NAME);
     if (user === null) {
       return "";
     } else {
@@ -39,11 +57,11 @@ class AuthenticationService {
     }
   }
 
-  setupAxiosInterceptors(basicAuthHeader) {
+  setupAxiosInterceptors(token) {
     //adds an auth header to every request
     axios.interceptors.request.use((config) => {
       if (this.isUserLoggedIn()) {
-        config.headers.authorization = basicAuthHeader;
+        config.headers.authorization = token;
       }
       return config;
     });
